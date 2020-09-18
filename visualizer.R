c0ddfdd420837eb0616a214d97a54133527cba66
hgp297 <hgp@berkeley.edu>
2020-09-18 16:35:15 -0500

    new set of runs with st1 st2

--- visualizer.R
+++ visualizer.R
@@ -9,7 +9,7 @@ isolDat <- read.csv('./sessionOut/sessionSummary.csv', header=TRUE)
 isolDat$maxDrift <- pmax(isolDat$driftMax1, isolDat$driftMax2, isolDat$driftMax3)
 
 isolPlot <- ggplot(isolDat, aes(GMSavg, maxDisplacement, colour = driftMax2, size = R1)) +
-  geom_point()
+  geom_smooth() + geom_point()
 
 isolPlot
 
@@ -18,12 +18,18 @@ intensityMeasures <- ggplot(isolDat, aes(GMSavg, GMS1, colour = S1, size = moatG
 
 intensityMeasures
 
-nonDimPlot <- ggplot(isolDat, aes(Pi3, Pi4, colour = maxDrift, size = maxDisplacement)) +
+nonDimPlot <- ggplot(isolDat, aes(RI, maxDisplacement, colour = maxDrift, size = moatGap)) +
   geom_point()
 
 nonDimPlot
 
 gapPlot <- ggplot(isolDat, aes(moatAmpli, maxDrift, colour = moatGap, size = maxDisplacement)) +
-  geom_point()
+  geom_point(aes(shape=impacted)) + scale_shape_identity()
 
 gapPlot
+
+bp <- ggplot(data = isolDat, aes(x = as.factor(impacted), y = moatAmpli)) +
+  geom_boxplot() +
+  xlab("Impacted?") + ylab("Moat amplification")
+
+bp
